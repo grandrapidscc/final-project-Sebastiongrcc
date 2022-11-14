1cd88beec267cdc160b664316ab780fd768645f4
Sebastiongrcc <95943524+Sebastiongrcc@users.noreply.github.com>
2022-11-14 09:14:06 -0500

    first draft
    The methods in program will be moved to its own file, I still struggle with that. for now I just wanted to make sure they were working

---
+++ CIS129FinalProject/Characters.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS129FinalProject { 
+    internal class Characters
+
+{
+    void Goblin()
+    {
+      
+        int hp1 = 3;
+        int damage1 = 2;
+
+    }
+    void Orc()
+    {
+        
+        int hp1 = 5;
+        int damage1 = 3;
+
+    }
+
+    void Banshee()
+    {
+        
+        int hp1 = 8;
+        int damage1 = 5;
+
+    }
+
+    void Wizart()
+    {
+        int health1 = 100;
+        int mp1 = 200;
+        int fbAtkDanage = 5;
+        int mgkHealDamage = health1 + 5;
+    }
+
+}
+}
--- CIS129FinalProject/Program.cs
+++ CIS129FinalProject/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// creating dungeon 
+int[,] dungeon = new int[5, 5] { {1,2,3,4,5}, 
+                                 {6,7,8,9,10}, 
+                                 {11,12,13,14,15}, 
+                                 {16,17,18,19,20}, 
+                                 {21,22,23,24,25}};
+
+
+shuffleArray(dungeon);
+int startingPoint;
+
+// i think this has to be in the actual game loop for a new random spot?
+for(int i = 0; i<1; i++)
+{
+    for(int j = 0; j < 1; j++)
+    {
+        startingPoint = dungeon[i,j];
+        Console.WriteLine(startingPoint);
+        
+    }  
+}
+
+
+
+
+
+void shuffleArray(int[,] arr)
+{
+    int n = arr.GetLength(0);
+    int m = arr.GetLength(1);
+
+    Random rand = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            swap(arr, i + rand.Next(n - i), j + rand.Next(m - j), i, j);
+        }
+    }
+
+}
+ void swap(int[,] arr, int changed, int changedB, int a, int b)
+{
+
+    int temp = arr[a, b];
+    arr[a, b] = arr[changed, changedB];
+    arr[changed, changedB] = temp;
+   
+}
